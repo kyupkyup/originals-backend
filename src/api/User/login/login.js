1133905b3d3f88cdd4d98373b32e1e5daf8d0a8d
kyupkyup <rudduqdl@naver.com>
2020-02-23 01:20:47 +0900

    url

--- src/api/User/login/login.js
+++ src/api/User/login/login.js
@@ -7,13 +7,14 @@ export default {
       const { email, password } = args;
       const user = await prisma.user({ email });
       const exists = await prisma.$exists.user({ email });
+      console.log(user);
       if (!exists) {
-        throw error("등록된 이메일이 아닙니다.");
+        throw Error("등록된 이메일이 아닙니다.");
       }
       if (user.password === password) {
         return generateToken(user.id);
       } else {
-        throw error("비밀번호가 틀렸습니다.");
+        throw Error("비밀번호가 틀렸습니다.");
       }
     }
   }

20e4b4de17ee3370f33d937baa1b11530f5ffa51
kyupkyup <rudduqdl@naver.com>
2020-02-23 04:19:19 +0900

    aasdas

--- src/passport.js
+++ src/passport.js
@@ -1,41 +1,39 @@
 import dotenv from "dotenv";
-import path from "path"
-import "./env"
+import path from "path";
+import "./env";
 
 import passport from "passport";
-import {Strategy, ExtractJwt} from "passport-jwt";
-import { prisma } from "../originals-demo/generated/prisma-client";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
 };
 
-const verifyUser = async(payload, done) =>{
-    try{
-        const user = await prisma.user({
-            id: payload.id
-        })
-        if(user !==null){
-            return done(null, user)
-        }else{
-            return done(null, false)
-        }
+const verifyUser = async (payload, done) => {
+  try {
+    const user = await prisma.user({
+      id: payload.id
+    });
+    if (user !== null) {
+      return done(null, user);
+    } else {
+      return done(null, false);
     }
-    catch(error){
-        return done(error, false)
-    }
-}
-
+  } catch (error) {
+    return done(error, false);
+  }
+};
 
 export const authenticateJwt = (req, res, next) => {
-    passport.authenticate("jwt", {session:false}, (error, user) => {
-        if(user){
-            req.user = user;
-        }
-        next();
-    })(req, res, next);
-}
+  passport.authenticate("jwt", { session: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+};
 passport.use(new Strategy(jwtOptions, verifyUser));
 passport.initialize();
 // {Authorization: 'Bearer TOKEN'}

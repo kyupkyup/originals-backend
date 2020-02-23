df7d4dfd8dd48868b3936be341293945bbffdcb8
kyupkyup <rudduqdl@naver.com>
2020-02-23 11:15:33 +0900

    Asd

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -6,6 +6,7 @@ export default {
     me: async (_, __, { request }) => {
       isAuthenticated(request);
       const { user } = request;
+      console.log(user);
       return await prisma.user({ id: user.id });
     }
   }

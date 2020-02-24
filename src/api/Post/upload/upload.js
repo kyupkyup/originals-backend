586f4cc9ae56a53cdedfabe34e0b62ee49da174c
kyupkyup <rudduqdl@naver.com>
2020-02-24 21:08:49 +0900

    deploy

--- src/api/Post/upload/upload.js
+++ src/api/Post/upload/upload.js
@@ -19,7 +19,7 @@ export default {
         main,
         announcement,
         title,
-        bulletinList: { connect: { id: "ck6giclaab9ok09196ffqz9hw" } }
+        bulletinList: { connect: { id: "ck70f19wn001r07467vn47v6s" } }
       });
       console.log(main);
       //   files.forEach(

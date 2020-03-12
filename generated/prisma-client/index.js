4fb87d30ea451e57a7aca7d8ac644314b907ce0c
kyupkyup <rudduqdl@naver.com>
2020-03-12 20:17:12 +0900

    delpoy v1.1.0

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -56,6 +56,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://eu1.prisma.sh/originals/originals/dev`
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`
 });
 exports.prisma = new exports.Prisma();
